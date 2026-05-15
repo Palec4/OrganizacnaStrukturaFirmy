9c1164c8359bb50aedbcc529aa5195f454f343d6
Palec4 <nemcekpavol294@gmail.com>
2026-05-15 12:07:52 +0200

    Add DivisionRepository

--- OrganizacnaStrukturaFirmy/Repository/Implementation/DivisionRepository.cs
+++ OrganizacnaStrukturaFirmy/Repository/Implementation/DivisionRepository.cs
@@ -1,6 +1,47 @@
-﻿namespace OrganizacnaStrukturaFirmy.Repository.Implementation
+﻿using Microsoft.EntityFrameworkCore;
+using OrganizacnaStrukturaFirmy.Data;
+using OrganizacnaStrukturaFirmy.Models;
+
+namespace OrganizacnaStrukturaFirmy.Repository.Implementation
 {
     public class DivisionRepository
     {
+        private readonly AppDbContext _context;
+
+        public DivisionRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Division>> GetAllAsync()
+            => await _context.Divisions.ToListAsync();
+
+        public async Task<IEnumerable<Division>> GetByCompanyAsync(int companyId)
+            => await _context.Divisions.Where(d => d.CompanyId == companyId).ToListAsync();
+
+        public async Task<Division?> GetByIdAsync(int id)
+            => await _context.Divisions.FindAsync(id);
+
+        public async Task<bool> CodeExistsAsync(string code, int? excludeId = null)
+            => await _context.Divisions.AnyAsync(d => d.Code == code && d.Id != excludeId);
+
+        public async Task<Division> CreateAsync(Division division)
+        {
+            _context.Divisions.Add(division);
+            await _context.SaveChangesAsync();
+            return division;
+        }
+
+        public async Task<Division> UpdateAsync(Division division)
+        {
+            await _context.SaveChangesAsync();
+            return division;
+        }
+
+        public async Task DeleteAsync(Division division)
+        {
+            _context.Divisions.Remove(division);
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- OrganizacnaStrukturaFirmy/Repository/Interface/IDivisionRepository.cs
+++ OrganizacnaStrukturaFirmy/Repository/Interface/IDivisionRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace OrganizacnaStrukturaFirmy.Repository.Interface
+﻿using OrganizacnaStrukturaFirmy.Models;
+
+namespace OrganizacnaStrukturaFirmy.Repository.Interface
 {
     public interface IDivisionRepository
     {
+        Task<IEnumerable<Division>> GetAllAsync();
+        Task<IEnumerable<Division>> GetByCompanyAsync(int companyId);
+        Task<Division?> GetByIdAsync(int id);
+        Task<bool> CodeExistsAsync(string code, int? excludeId = null);
+        Task<Division> CreateAsync(Division division);
+        Task<Division> UpdateAsync(Division division);
+        Task DeleteAsync(Division division);
     }
 }
